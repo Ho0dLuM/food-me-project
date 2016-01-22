ac860567223d3e3afafeb6feab1de6c1fa61e8f7
a25patel <aanaletap2510@gmail.com>
2016-01-21 21:28:48 -0700

    splash page to restaurants index, and index to new form routes complete

--- restaurants/routes/restaurants.js
+++ restaurants/routes/restaurants.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('restaurants/index');
 });
 
+//GET new restaurant form
+router.get('/new', function(req,res,next){
+  res.render('restaurants/new');
+});
+
+//POST CREATE a new restaurant
 module.exports = router;
