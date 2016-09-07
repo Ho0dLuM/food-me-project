292ff0a9432ea511bde22e75bec26aa220e0bf23
Wes Reid <bwreid@gmail.com>
2016-09-07 14:12:38 -0600

    Include faker data in restaurants

--- src/server/db/seeds/01_restaurants.js
+++ src/server/db/seeds/01_restaurants.js
@@ -1,4 +1,6 @@
 // CUISINE_TYPES = [ 'american', 'italian', 'mexican', 'thai' ]
+const _ = require('lodash')
+const faker = require('faker')
 
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
@@ -8,21 +10,21 @@ exports.seed = function (knex, Promise) {
         // Inserts seed entries
         knex('restaurants').insert({
           id: 1,
-          name: 'The Best Little American Restaurant',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          cuisine_type: 0
+          name: `The ${_.capitalize(faker.company.bsAdjective())} Restaurant`,
+          description: faker.lorem.paragraph(),
+          cuisine_type: faker.random.number(2)
         }),
         knex('restaurants').insert({
           id: 2,
-          name: 'The Best Little Italian Restaurant',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          cuisine_type: 1
+          name: `The ${_.capitalize(faker.company.bsAdjective())} Restaurant`,
+          description: faker.lorem.paragraph(),
+          cuisine_type: faker.random.number(2)
         }),
         knex('restaurants').insert({
           id: 3,
-          name: 'The Best Little Mexican Restaurant',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          cuisine_type: 2
+          name: `The ${_.capitalize(faker.company.bsAdjective())} Restaurant`,
+          description: faker.lorem.paragraph(),
+          cuisine_type: faker.random.number(2)
         })
       ])
     })
