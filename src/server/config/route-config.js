1676c0e73078f4788f8678aca69dd6e775fc6736
Wes Reid <bwreid@gmail.com>
2016-09-07 08:44:55 -0600

    Add restaurants index route and template

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -6,9 +6,11 @@
 
     // *** routes *** //
     const routes = require('../routes/index');
+    const restaurants = require('../routes/restaurants');
 
     // *** register routes *** //
     app.use('/', routes);
+    app.use('/restaurants', restaurants);
 
   };
 
