2c97a0b8f595af31c3401ddfd584f3a7cabdc9ed
Wes Reid <bwreid@gmail.com>
2016-09-10 14:39:16 -0600

    Update main styling for homepage

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,16 +1,1 @@
-(function () {
-  var faded = false;
-  var $h3 = $('#review h3')
-  var $p = $('#review p')
-
-  $h3.css({ opacity: 1 })
-  $p.css({ opacity: 0.15 })
-
-  $(window).on('scroll', function () {
-    if (!faded) {
-      $h3.animate({ opacity: 0.15 }, 1000)
-      $p.animate({ opacity: 1 }, 1000)
-      faded = true
-    }
-  })
-})()
+(function () {})()
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const { Restaurant } = require('../db')
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Welcome to gRestaurants' })
+  Restaurant.get().limit(3)
+  .then(Restaurant.getAddresses)
+  .then(restaurants => {
+    let cuisines = [ 'american', 'italian', 'mexican', 'thai' ]
+    restaurants = restaurants.map(restaurant => {
+      restaurant.cuisine = cuisines[restaurant.cuisine_type]
+      return restaurant
+    })
+    res.render('index', { restaurants })
+  })
 })
 
 module.exports = router
