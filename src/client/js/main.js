5503e1bdb5d4ad288c78d86a133620b18e7787db
Wes Reid <bwreid@gmail.com>
2016-09-11 11:52:44 -0600

    Add delete links for Employees and Restaurants

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -7,6 +7,38 @@
     }
   })
 
+  $('[id^="delete-"]').on('click', function (e) {
+    e.preventDefault()
+    let self = this
+    let $button = $(this)
+    let url = $button.attr('href')
+    let type = 'DELETE'
+
+    $.ajax({ url, type })
+      .success(function (msg) {
+        if ($button.data().deleteRedirect) deleteRedirect(url)
+        if ($button.data().deleteTable) deleteTable.apply(self)
+      })
+      .fail(function (msg) {
+        console.log('Failure!', msg)
+      })
+  })
+
+  function deleteRedirect (url) {
+    let parent = url.split('/')
+    parent.pop()
+
+    window.location.pathname = parent.join('/')
+  }
+
+  function deleteTable () {
+    $tbody = $(this).closest('tbody')
+    $(this).closest('tr').remove()
+    console.log($tbody)
+
+    if (!$tbody.children().length) $tbody.closest('.row').remove()
+  }
+
   function removeActive () {
     $('.active').children('.truncated').show()
     $('.active').children('.full-content').hide()
