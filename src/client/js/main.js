e543da9b3752486135de0336d48fd99ef6574765
Wes Reid <bwreid@gmail.com>
2016-09-10 19:19:32 -0600

    Show page initialized
    * Show all relevant information on the page
    * Split rating partial into multiple partials
    * Add jQuery magic

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,1 +1,22 @@
-(function () {})()
+(function ($) {
+  $('.review').on('click', function () {
+    if (this === $('.active')[0]) {
+      removeActive()
+    } else {
+      addActive.apply(this)
+    }
+  })
+
+  function removeActive () {
+    $('.active').children('.truncated').show()
+    $('.active').children('.full-content').hide()
+    $('.active').removeClass('active')
+  }
+
+  function addActive () {
+    removeActive()
+    $(this).addClass('active')
+    $(this).children('.truncated').hide()
+    $(this).children('.full-content').show()
+  }
+})(window.$)
