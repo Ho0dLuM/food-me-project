06d5963435345603bd7756e5b9bb4b28709ed787
Wes Reid <bwreid@gmail.com>
2016-09-11 20:32:16 -0600

    Add Auth model for middleware

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,6 +1,7 @@
 const knex = require('./connection')
 
 module.exports = {
+  Auth: require('./models/auth'),
   Account: require('./models/account'),
   Address: require('./models/address'),
   Employee: require('./models/employee'),
---
+++ src/server/db/models/auth.js
@@ -0,0 +1,25 @@
+module.exports = {
+  noLoginRequired: (req, res, next) => {
+    if (req.user) {
+      res.redirect('/restaurants')
+    } else {
+      next()
+    }
+  },
+  loginRequired: (req, res, next) => {
+    if (!req.user) {
+      req.flash('error', 'You must login to reach that page.')
+      res.redirect('/login')
+    } else {
+      next()
+    }
+  },
+  adminRequired: (req, res, next) => {
+    if (!req.user.admin) {
+      req.flash('error', 'Sorry, you don\'t have access to that page')
+      res.redirect('/restaurants')
+    } else {
+      next()
+    }
+  }
+}
